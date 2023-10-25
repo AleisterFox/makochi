37616e90e20f91755d72a9fc8070879dcef36923
Interlab Interlab <ericaguirrez@hotmail.com>
2023-10-25 15:27:46 -0600

    add us section

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Fondo Animado</title>
+</head>
+
+<body>
+    <header></header>
+    <main>
+        <section id="hero">
+
+            <canvas  id="container">
+                <img src="img/Fondo.jpg" alt="">
+
+            </canvas>
+
+            <div class="container">
+                <h1>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h1>
+                <a href="#" class="button green">Agenda tu cita</a>
+            </div>
+
+            <div class="sheet">
+                <h3>Inscripciones abiertas</h3>
+                <a href="#" class="button yellow">Ver más</a>
+            </div>
+
+            <div class="pet">
+                <img src="img/Mascota.png" alt="">
+            </div>
+
+        </section>
+    </main>
+    <footer></footer>
+
+    <script src="js/particles.js"></script>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
